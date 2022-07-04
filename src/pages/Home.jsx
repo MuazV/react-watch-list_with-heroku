020006e30e380ced8e7eaec2116c4164276196b9
muaz <muaz.vakitsayan@gmail.com>
2022-07-04 05:48:29 +0300

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AddTutorial from '../components/AddTutorials'
 import TutorialList from '../components/TutorialList';
+import axios from 'axios';
 
 const Home = () => {
+    const [tutorials, setTutorials] = useState();
     
+    const url = 'https://tutorials-api-cw.herokuapp.com/api/tutorials';
+
+    //! GET (Read)
+    const getTutorials = async() => {
+        try {
+        const {data} = await axios.get(url)
+        setTutorials(data);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getTutorials()
+    }, [])
+
+
+
   return (
     <div>
         <AddTutorial/>
-        <TutorialList/>
+        <TutorialList tutorials = {tutorials}/>
     </div>
   )
 }
